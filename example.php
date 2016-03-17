df654b4eddbaae6de7d5e9ed5af505bd072e0670
WesamMikhail <wesam@lorenum.com>
2016-03-17 13:16:47 +0100

    changed email in example from mine to example email

--- example.php
+++ example.php
@@ -6,7 +6,7 @@ use Lorenum\ScriptCheck\Handlers\EmailHandler;
 
 $sc = new ScriptCheck();
 $sc->addHandler(new \Lorenum\ScriptCheck\Handlers\FileLoggerHandler("log.log"));
-$sc->addHandler(new EmailHandler("wesam@lorenum.com", "your script got fucked!")); #the error will also be emailed to you
+$sc->addHandler(new EmailHandler("myemail@provider.com", "your script got fucked!")); #the error will also be emailed to you
 $sc->register();
 
 
