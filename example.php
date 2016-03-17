fdfb05c93e55ef084c7ddd5c1dc982a8c3899316
WesamMikhail <wesam@lorenum.com>
2016-03-17 13:15:52 +0100

    Added Example Script

---
+++ example.php
@@ -0,0 +1,13 @@
+<?php
+require_once "vendor/autoload.php";
+
+use Lorenum\ScriptCheck\ScriptCheck;
+use Lorenum\ScriptCheck\Handlers\EmailHandler;
+
+$sc = new ScriptCheck();
+$sc->addHandler(new \Lorenum\ScriptCheck\Handlers\FileLoggerHandler("log.log"));
+$sc->addHandler(new EmailHandler("wesam@lorenum.com", "your script got fucked!")); #the error will also be emailed to you
+$sc->register();
+
+
+throw new Exception("Test Exception");
